c77ca3550b8895280cae69033ddfc1db9efee264
Muneeb ul Hassan <smati.muneeb@gmail.com>
2016-11-15 19:57:00 +1100

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,39 @@
+
+import java.io.*;
+import java.util.*;
+
+public class InsertionSort {
+
+    public static void insertionSortPart2(int[] ar)
+    {       
+           for(int i=1; i<ar.length; i++){
+               for(int j=0; j<i;j++){  
+                    if(ar[i]<ar[j]){
+                    int temp = ar[j];
+                    ar[j] = ar[i];
+                    ar[i] = temp;
+                    }
+                }
+               printArray(ar);
+           }
+    }  
+    
+    
+      
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       int s = in.nextInt();
+       int[] ar = new int[s];
+       for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+       }
+       insertionSortPart2(ar);    
+                    
+    }    
+    private static void printArray(int[] ar) {
+      for(int n: ar){
+         System.out.print(n+" ");
+      }
+        System.out.println("");
+   }
+}
